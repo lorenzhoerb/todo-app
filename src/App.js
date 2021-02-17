e418fe0e78e9149da0a183bb4290787e2648e51d
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2021-02-17 11:15:10 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import TodoForm from './Components/TodoForm/TodoForm.js'
+import TodoList from './Components/TodoList/TodoList.js'
+import { TodosContext } from './TodosContext'
+
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo's</h1>
+      <TodosContext.Provider value={{ todos, setTodos }}>
+        <TodoForm />
+        <TodoList />
+      </TodosContext.Provider>
     </div>
   );
 }
